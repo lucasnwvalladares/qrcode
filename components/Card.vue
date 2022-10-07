bc0998a3c96a5e31715566a74fe3ac0245b8ead4
lucasnwvalladares <lucasnwvalladares@gmail.com>
2022-10-07 11:31:40 -0300

    html basis

---
+++ components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="card">
+        <img src="/image-qr-code.png" alt="qrcode">
+        <h1>Improve your front-end skills by building projects</h1>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scope>
+    #card {
+        background: white;
+        font-family: 'Outfit', sans-serif;
+    }
+</style>
